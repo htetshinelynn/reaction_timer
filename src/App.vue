273e6bc5db4713181252835ed10f93c51df76474
htetshinelynn <htetshinelin.myanmar@gmail.com>
2021-01-30 18:11:35 +0630

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>How Fast Can u Catch me ?</h1>
+  <button @click="start" :disabled="isplaying">play</button>
+  <div v-if="isplaying">
+    <Block :delay="delay" @endGame="endGame"/>
+  </div>
+  <div v-if="end">
+    <Result :score="score" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from "./components/Block.vue";
+import Result from "./components/Result.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Block,Result
+  },
+  data(){
+    return {
+      isplaying:false,
+      delay:null,
+      score:0,
+      end:false
+    }
+    
+  },
+  methods:{
+    start(){
+      this.isplaying=true,
+      this.delay=2000+Math.random()*5000;
+    },
+    endGame(score){
+      this.score=score;
+      this.isplaying=false;
+      this.end=true;
+    }
   }
 }
 </script>
---
+++ src/components/Block.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class=" block" v-if="showBlock" @click="stoptimer">click here</div>
+</template>
+
+<script>
+export default {
+  props:['delay'],
+  data(){
+    return{
+      showBlock:false,
+      score:0,
+      timer:null
+    }
+  },
+  mounted(){
+    setTimeout(() => {
+      this.showBlock=true;
+      this.startTimer()
+    }, this.delay);
+  },
+  methods:{
+    startTimer(){
+      this.timer=setInterval(() => {
+        this.score+=50;
+      }, 50);
+    },
+    stoptimer(){
+      clearInterval(this.timer);
+      this.$emit("endGame",this.score)
+    }
+  },
+  updated(){
+    console.log("updated")
+  }
+  
+}
+</script>
+
+<style>
+    .block{
+        background-color: aqua;
+        padding:140px 30px;
+        width: 300px;
+        margin: 20px auto;
+        border-radius: 50%;
+    }
+</style>
